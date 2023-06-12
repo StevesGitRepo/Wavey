77abc5732aa269960e5d5f50dc3e01557b09e09c
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-06-11 21:42:38 -0700

    Extensions folder added including CustomAttributes & IdentityExtensions

---
+++ Extensions/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace HotBug.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+            //FindFirst gives immediate access to the first claim that matches the specified type
+            Claim claim = ((ClaimsIdentity)identity).FindFirst("CompanyId");
+            //Ternary operator (if/else)
+            return (claim != null) ? int.Parse(claim.Value) : null;
+        }
+    }
+}
