20320ad806e8e67eff28776b45bf5a86279a9f2e
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-06-12 17:09:08 -0700

    postgres migration code added/ app runs on localhost with this build

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ using HotBug.Services.Factories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 /*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
